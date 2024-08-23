640ac46572b9fdefbf569b5a8e08335ff38ca7e1
Raktim Biswas <raktimb181@gmail.com>
2024-08-23 20:05:58 +0530

    Input

--- main.py
+++ main.py
@@ -4,8 +4,17 @@
 # print("hello world")
 # first_name = "Raktim"
 # print(f"Hello {first_name}")
-student = False
-if student:
-    print("You are a student")
-else:
-    print("You are NOT a student")
+# student = False
+# if student:
+#     print("You are a student")
+# else:
+#     print("You are NOT a student")
+# name = "Rob stark"
+# age = 1000
+# weight = 9.7
+# warrior = True
+# print(type(age))
+# age = float(age)
+# print(age)
+name = input("What is your name: ")
+print(name)
