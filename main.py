2e90c399f3a5692a6671cc1bb294b2835c18c64c
Raktim Biswas <raktimb181@gmail.com>
2024-08-23 19:38:07 +0530

    variables

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import pyjokes
-jokes = pyjokes.get_joke()
-print(jokes)
-print("hello wrold")
+# import pyjokes
+# jokes = pyjokes.get_joke()
+# print(jokes)
+# print("hello world")
+# first_name = "Raktim"
+# print(f"Hello {first_name}")
+student = False
+if student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
