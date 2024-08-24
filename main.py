a050f6b2f1d14f744665ea8e549a150ede4d422b
Raktim Biswas <raktimb181@gmail.com>
2024-08-24 14:58:13 +0530

    Rectangle calculation

--- main.py
+++ main.py
@@ -16,5 +16,13 @@
 # print(type(age))
 # age = float(age)
 # print(age)
-name = input("What is your name: ")
-print(name)
+# name = input("What is your name: ")
+# age =int(input("What is your age: "))
+# age +=1
+# print(f"Happy Birthday {name}")
+# print(f"Your turned {age} Today!")
+#Rectangle calculation of an area
+length =float( input("Enter the length: "))
+width =float( input("Enter the width: "))
+area = length * width
+print(f"Your area is: {area}cm²")
