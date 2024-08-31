55bf896f89eb0956a0a3767335187d09dbdeef89
Raktim Biswas <raktimb181@gmail.com>
2024-08-31 11:02:56 +0530

    Validate user input

--- main.py
+++ main.py
@@ -71,3 +71,17 @@
 #conditional expressions
 # num = 981
 # print("Your have more time to explore the world"if num <100 else "You have to die")
+# validate user input
+username = input("Enter your username:")
+length =len(username)
+has_digit = any(char.isdigit() for char in username)
+has_space = ' ' in username
+if length < 12 and has_digit and not has_space:
+    print("username is valid!")
+else:
+    if length > 12:
+        print("Username must not be longer than 12 characters!")
+    if not has_digit:
+        print("Username must contain at least one number!")
+    if has_space:
+        print("Username must not contain spaces!")
