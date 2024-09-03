0dac81997030d6dfd2d58de2db47a7cc00a1e27e
Raktim Biswas <raktimb181@gmail.com>
2024-09-03 13:18:23 +0530

    Indexing

--- main.py
+++ main.py
@@ -72,16 +72,22 @@
 # num = 981
 # print("Your have more time to explore the world"if num <100 else "You have to die")
 # validate user input
-username = input("Enter your username:")
-length =len(username)
-has_digit = any(char.isdigit() for char in username)
-has_space = ' ' in username
-if length < 12 and has_digit and not has_space:
-    print("username is valid!")
-else:
-    if length > 12:
-        print("Username must not be longer than 12 characters!")
-    if not has_digit:
-        print("Username must contain at least one number!")
-    if has_space:
-        print("Username must not contain spaces!")
+# username = input("Enter your username:")
+# length =len(username)
+# has_digit = any(char.isdigit() for char in username)
+# has_space = ' ' in username
+# if length < 12 and has_digit and not has_space:
+#     print("username is valid!")
+# else:
+#     if length > 12:
+#         print("Username must not be longer than 12 characters!")
+#     if not has_digit:
+#         print("Username must contain at least one number!")
+#     if has_space:
+#         print("Username must not contain spaces!")
+#Indexing
+debit_number = "5649-6546-6556-5454"
+print(debit_number)
+print(debit_number[0:4])
+print(debit_number[:4])
+print(debit_number[0:])
