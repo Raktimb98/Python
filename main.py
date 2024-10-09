6ae7cc3e13b14019e684502d5213a997b8130f44
Raktim Biswas <raktimb181@gmail.com>
2024-10-09 19:28:48 +0530

    Compound interest calculator

--- main.py
+++ main.py
@@ -115,8 +115,33 @@
 # print ("See you again boi !")
 
 #Another one
-num = int(input("Enter a number between 1 to 10:"))
-while num < 1 or num > 10:
-    print(f"{num} is invalid")
-    num = int(input("Enter a number between 1 to 10:"))
-print (f"Your number is: {num}")
+# num = int(input("Enter a number between 1 to 10:"))
+# while num < 1 or num > 10:
+#     print(f"{num} is invalid")
+#     num = int(input("Enter a number between 1 to 10:"))
+# print (f"Your number is: {num}")
+
+#Compound interest calculator
+principle = 0
+rate = 0
+time = 0
+principle = float(input("Enter the principle amount:"))
+while principle <= 0 :
+    principle = float(input("Enter the principle amount:"))
+    if principle <=0:
+        print("Principle can't be negative or zero !")
+
+rate = float(input("Enter the rate amount:"))
+while rate <= 0 :
+    rate = float(input("Enter the rate amount:"))
+    if rate <=0:
+        print("rate can't be negative or zero !")
+
+time = int(input("Enter the time in years:"))
+while time <= 0 :
+    time = int(input("Enter the time in years:"))
+    if time <=0:
+        print("time can't be negative or zero !")
+
+total = principle * pow((1+rate/100),time)
+print(f"Total amount after {time} years is: {total:.2f}")
