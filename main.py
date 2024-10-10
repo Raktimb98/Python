1e8edf5c69b00d9d8f57f55abdde01aeff7ddb07
Raktim Biswas <raktimb181@gmail.com>
2024-10-10 14:39:28 +0530

    For loops

--- main.py
+++ main.py
@@ -122,26 +122,38 @@
 # print (f"Your number is: {num}")
 
 #Compound interest calculator
-principle = 0
-rate = 0
-time = 0
-principle = float(input("Enter the principle amount:"))
-while principle <= 0 :
-    principle = float(input("Enter the principle amount:"))
-    if principle <=0:
-        print("Principle can't be negative or zero !")
+# principle = 0
+# rate = 0
+# time = 0
+# principle = float(input("Enter the principle amount:"))
+# while principle <= 0 :
+#     principle = float(input("Enter the principle amount:"))
+#     if principle <=0:
+#         print("Principle can't be negative or zero !")
 
-rate = float(input("Enter the rate amount:"))
-while rate <= 0 :
-    rate = float(input("Enter the rate amount:"))
-    if rate <=0:
-        print("rate can't be negative or zero !")
+# rate = float(input("Enter the rate amount:"))
+# while rate <= 0 :
+#     rate = float(input("Enter the rate amount:"))
+#     if rate <=0:
+#         print("rate can't be negative or zero !")
 
-time = int(input("Enter the time in years:"))
-while time <= 0 :
-    time = int(input("Enter the time in years:"))
-    if time <=0:
-        print("time can't be negative or zero !")
+# time = int(input("Enter the time in years:"))
+# while time <= 0 :
+#     time = int(input("Enter the time in years:"))
+#     if time <=0:
+#         print("time can't be negative or zero !")
 
-total = principle * pow((1+rate/100),time)
-print(f"Total amount after {time} years is: {total:.2f}")
+# total = principle * pow((1+rate/100),time)
+# print(f"Total amount after {time} years is: {total:.2f}")
+
+#For loops
+for n in range(0,11):
+    print(n)
+print("Hello youth")
+
+for x in reversed(range(0,11)):
+    print(x)
+
+customer_id="154984-854-89498-84-56445"
+for x in customer_id:
+    print(x)
