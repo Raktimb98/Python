0f747febe9be4b733100db161d1f6da22debac59
Raktim Biswas <raktimb181@gmail.com>
2024-10-29 19:37:49 +0530

    Random module

--- main.py
+++ main.py
@@ -141,7 +141,15 @@
 #     print()
 
 #Dictionaries
-student = {"Ram":"marks-95","Sham":"marks- 54","Jodu":"marks-34","yash":"marks-100"}
+# student = {"Ram":"marks-95","Sham":"marks- 54","Jodu":"marks-34","yash":"marks-100"}
 # print(student.get("Jodu"))
-student.update({"ayesha":"marks-99"})
-print(student.get("ayesha"))
+# student.update({"ayesha":"marks-99"})
+# print(student.get("ayesha"))
+
+#Random module
+import random
+# print(help(random))
+low = 1
+high = 1000
+number = random.randint(low ,high)
+print(number)
