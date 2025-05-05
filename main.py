e018a5cb6959bbba45ec056eb5957dc035711ef3
Raktim Biswas <raktimb181@gmail.com>
2025-05-05 19:30:33 +0530

    PyQt5 Button

--- main.py
+++ main.py
@@ -498,60 +498,88 @@
 #     main()
 
 #* PyQt5 layouts
+# import sys
+# from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QHBoxLayout,QGridLayout
+
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setGeometry(500, 250, 500, 300)
+#         self.initUI()
+
+#     def initUI(self):
+#         central_widget = QWidget()
+#         self.setCentralWidget(central_widget)
+#         label1 = QLabel("Label 1", self)
+#         label2 = QLabel("Label 2", self)
+#         label3 = QLabel("Label 3", self)
+#         label4 = QLabel("Label 4", self)
+#         label5 = QLabel("Label 5", self)
+
+#         label1.setStyleSheet("background-color: red;")
+#         label2.setStyleSheet("background-color: green;")
+#         label3.setStyleSheet("background-color: blue;")
+#         label4.setStyleSheet("background-color: yellow;")
+#         label5.setStyleSheet("background-color: orange;")
+
+#         # vbox = QVBoxLayout()
+#         # vbox.addWidget(label1)
+#         # vbox.addWidget(label2)
+#         # vbox.addWidget(label3)
+#         # vbox.addWidget(label4)
+#         # vbox.addWidget(label5)
+#         # central_widget.setLayout(vbox)
+
+#         # hbox = QHBoxLayout()
+#         # hbox.addWidget(label1)
+#         # hbox.addWidget(label2)
+#         # hbox.addWidget(label3)
+#         # hbox.addWidget(label4)
+#         # hbox.addWidget(label5)
+#         # central_widget.setLayout(hbox)
+
+#         grid = QGridLayout()
+#         grid.addWidget(label1, 0, 0)
+#         grid.addWidget(label2, 0, 1)
+#         grid.addWidget(label3, 1, 0)
+#         grid.addWidget(label4, 1, 1)
+#         grid.addWidget(label5, 2, 0, 1, 2)
+#         central_widget.setLayout(grid)
+
+# def main():
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.setWindowTitle("Python GUI")
+#     window.show()
+#     sys.exit(app.exec_())
+
+# if __name__ == "__main__":
+#     main()
+
+#* PyQt5 Buttons
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QHBoxLayout,QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(500, 250, 500, 300)
-        self.initUI()
+        self.button = QPushButton("Click Me", self)
+        self.InitUI()
 
-    def initUI(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        label1 = QLabel("Label 1", self)
-        label2 = QLabel("Label 2", self)
-        label3 = QLabel("Label 3", self)
-        label4 = QLabel("Label 4", self)
-        label5 = QLabel("Label 5", self)
+    def InitUI(self):
+        self.button.setGeometry(50, 50, 100, 30)
+        self.button.setStyleSheet("background-color: green; color: white; font-size: 16px;")
+        self.button.clicked.connect(self.on_click)
 
-        label1.setStyleSheet("background-color: red;")
-        label2.setStyleSheet("background-color: green;")
-        label3.setStyleSheet("background-color: blue;")
-        label4.setStyleSheet("background-color: yellow;")
-        label5.setStyleSheet("background-color: orange;")
-
-        # vbox = QVBoxLayout()
-        # vbox.addWidget(label1)
-        # vbox.addWidget(label2)
-        # vbox.addWidget(label3)
-        # vbox.addWidget(label4)
-        # vbox.addWidget(label5)
-        # central_widget.setLayout(vbox)
-
-        # hbox = QHBoxLayout()
-        # hbox.addWidget(label1)
-        # hbox.addWidget(label2)
-        # hbox.addWidget(label3)
-        # hbox.addWidget(label4)
-        # hbox.addWidget(label5)
-        # central_widget.setLayout(hbox)
-
-        grid = QGridLayout()
-        grid.addWidget(label1, 0, 0)
-        grid.addWidget(label2, 0, 1)
-        grid.addWidget(label3, 1, 0)
-        grid.addWidget(label4, 1, 1)
-        grid.addWidget(label5, 2, 0, 1, 2)
-        central_widget.setLayout(grid)
-
-def main():
-    app = QApplication(sys.argv)
+    def on_click(self):
+        print   ("Button clicked")
+        self.button.setText("Clicked")
+        self.button.setDisabled(True)
+if __name__ == "__main__":
+    app = QApplication(sys.argv) 
     window = MainWindow()
     window.setWindowTitle("Python GUI")
     window.show()
     sys.exit(app.exec_())
 
-if __name__ == "__main__":
-    main()
